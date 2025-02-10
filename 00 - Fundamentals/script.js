4aa4f1c1cbab69529edfa20f4554db236a65c176
harisdev-netizen <mharispk321@gmail.com>
2025-02-10 22:36:33 +0500

    feat:

--- 00 - Fundamentals/script.js
+++ 00 - Fundamentals/script.js
@@ -1,7 +1,8 @@
+// Variable is basically a box, in which we can store a value, so we give that box a name, and then later in the code we can reference that variable name over and over again.
+
+// In below examples 'firstName', 'x', 'y' and 'z' are called variables, and here we have named the variables a 'firstName', 'x', 'y', 'z'.
 let firstName = 'Haris';
 console.log(firstName);
-
-// In these two examples 'firstName' and 'x' are called variables, and here we have named the variables a 'firstName' and 'x'.
 
 let x = 5 + 3;
 console.log(x);
@@ -10,3 +11,28 @@ let y = 4;
 let z = 3;
 let result = y + z;
 console.log(result);
+
+// We should always give a useful and understandable names to our variables for example:
+let myFirstJob = 'Teacher';
+let mySecondJob = 'Frontend Developer';
+
+console.log(myFirstJob);
+console.log(mySecondJob);
+
+// TASK
+let country = 'Pakistan';
+let continent = 'South Asia';
+let population = 257047044;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// THE 7 PRIMITIVE DATA TYPES:
+
+// 1. Number: Floating point numbers, used for decimals and integers
+let age = 24;
+
+// 2. String: Sequence of Characters, used for text
+let secondName = 'Haris';
