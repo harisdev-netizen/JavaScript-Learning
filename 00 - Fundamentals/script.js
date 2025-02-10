c2f03545a98564374ba0f71af6c8caee485a4e05
harisdev-netizen <mharispk321@gmail.com>
2025-02-10 15:10:27 +0500

    first commit

---
+++ 00 - Fundamentals/script.js
@@ -0,0 +1,12 @@
+let firstName = 'Haris';
+console.log(firstName);
+
+// In these two examples 'firstName' and 'x' are called variables, and here we have named the variables a 'firstName' and 'x'.
+
+let x = 5 + 3;
+console.log(x);
+
+let y = 4;
+let z = 3;
+let result = y + z;
+console.log(result);
