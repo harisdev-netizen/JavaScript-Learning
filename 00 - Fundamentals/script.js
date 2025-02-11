9a1a0b0bfb1c93633693566224240ff3dac05c93
harisdev-netizen <mharispk321@gmail.com>
2025-02-11 21:55:20 +0500

    feat:

--- 00 - Fundamentals/script.js
+++ 00 - Fundamentals/script.js
@@ -33,6 +33,18 @@ console.log(population);
 
 // 1. Number: Floating point numbers, used for decimals and integers
 let age = 24;
+console.log( typeof age);
 
 // 2. String: Sequence of Characters, used for text
 let secondName = 'Haris';
+console.log(typeof secondName);
+
+// 3. Boolean: Logical type that can only be used "true" and "false", used for taking decisons.
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// 4. Undefined: Value taken by a variable that is not yet defined ('empty value')
+// 5. Null: Also means ('empty value')
+// 6. Symbol (ES2015): Value that is unique and cannot be changed.
+// 7. BigInt (ES2022): Larger integers than the 'Number' type can hold.
+
